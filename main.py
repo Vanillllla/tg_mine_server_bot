ee8f73af0ab611583347e0196bbb04b26e3a08a2
Vanillllla <ivan.mavritskiy@gmail.com>
2026-01-19 22:39:25 +0300

    0.0.4 add trey menu, normal exit func, program ico

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 # Способ 1: Динамическая загрузка
+import os
+import subprocess
+import sys
+
+
+
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QMessageBox
-
-import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+from PyQt5.QtWidgets import QMainWindow, QLabel
+from PyQt5.QtWidgets import QSystemTrayIcon, QMenu, QAction
+from PyQt5.QtGui import QIcon
 
 from upload_window import UploadWindow
 
@@ -12,30 +18,36 @@ class MyApp(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('main_ui.ui', self)
-        self.setWindowTitle("Servers controller")
+        self.setWindowTitle("Servers Telegram controller")
+        self.setWindowIcon(QIcon('icon.ico'))
 
 
-        self.upload_core_action.triggered.connect(self.open_upload_window)
+        self.tray_icon = QSystemTrayIcon(self)
+        self.tray_icon.setIcon(QIcon("icon_tray.ico"))  # или QIcon.fromTheme()
+        traymenu = QMenu()
+        traymenu.addAction("Открыть").triggered.connect(self.show)
+        traymenu.addAction("Выход").triggered.connect(self.close_program)
+        self.tray_icon.setContextMenu(traymenu)
+
+
+        self.upload_core_action.triggered.connect(self.open_upload_cores_window)
 
 
         self.open_setings_action.triggered.connect(self.open_settings_window)
 
 
-        self.exit_action.triggered.connect(self.close)
-        self.to_trey_action.triggered.connect(self.to_trey_program)
+        self.exit_action.triggered.connect(self.close_program)
+        self.to_trey_action.triggered.connect(self.close)
         self.restart_action.triggered.connect(self.restart_program)
 
-
-
-
-
         self.initUI()
+
 
 
     def initUI(self):
         self.show()
 
-    def open_upload_window(self, to_path = None ):
+    def open_upload_cores_window(self ):
         """Открываем окно загрузки файлов"""
         self.upload_window = UploadWindow(self, "downloads_cores")  # self как родитель
         self.upload_window.show()
@@ -44,26 +56,33 @@ class MyApp(QMainWindow):
         print("open_settings_window")
 
     def restart_program(self):
-        print("restart_program")
+        """Полный перезапуск приложения"""
+        QMessageBox.warning(self, "Перезапуск", "Приложение будет перезапущено")
+        QApplication.quit()
+
+        os.execl(sys.executable, sys.executable, *sys.argv)
 
     def to_trey_program(self):
         print("to_trey_program")
 
     def close_program(self):
-        self.close()
+        # reply = QMessageBox.question(
+        #     self, 'Подтверждение',
+        #     'Вы уверены, что хотите выйти?',
+        #     QMessageBox.Yes | QMessageBox.No,
+        #     QMessageBox.No
+        # )
+        # if reply == QMessageBox.Yes:
+            QApplication.quit()   # Закрыть
 
     def closeEvent(self, event):
         # Здесь можно сохранить настройки, спросить подтверждение
-        reply = QMessageBox.question(
-            self, 'Подтверждение',
-            'Вы уверены, что хотите выйти?',
-            QMessageBox.Yes | QMessageBox.No,
-            QMessageBox.No
-        )
-        if reply == QMessageBox.Yes:
-            event.accept()  # Закрыть
-        else:
-            event.ignore()  # Не закрывать
+
+        self.hide()  # Скрываем окно
+        self.tray_icon.show()  # Показываем иконку в трее
+        event.ignore()  # Не закрываем программу
+
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
