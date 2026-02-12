9ef03afef8bcc459e14a04b1b0299e1413192d88
ivan-tablet <ivan.mavritsky@gmail.com>
2026-02-12 14:00:10 +0300

    0.2.14 server control from GUI (start/stop) button

--- server_system.py
+++ server_system.py
@@ -3,7 +3,6 @@ import json
 import subprocess
 import time
 import threading
-from os import name
 from pathlib import Path
 
 
@@ -21,52 +20,68 @@ class ServerSystem:
         while True:
             try:
                 msg = self.conn.recv()
-                # ans = {"to_process": "bot", "from_process": "server", "command": "set_server_status", "data": False,
-                #        "request_id": msg["request_id"]}
+
                 if msg["command"] == "set_server_status":
-                    if self.process is None:
+                    if not self.is_running():
                         self.start_server()
-                        ans = {"to_process": "gui", "from_process": "server", "command": "set_server_status",
-                           "data": True if self.process is not None else False}
+                        status = self.is_running()
                     else:
+                        self.is_running()
                         self.stop_server()
-                        ans = {"to_process": "gui", "from_process": "server", "command": "set_server_status",
-                           "data": True if self.process is not None else False}
+                        status = False
+
+                    ans = {"to_process": "gui", "from_process": "server", "command": "set_server_status",
+                           "data": status, "request_id": msg.get("request_id")}
                     self.conn.send(ans)
+
                 elif msg["command"] == "server_command":
-                    print("print --> ", self.conn, msg)
-                    self.send_commansds(msg["data"])
+                    self.send_commands(msg["data"])
+
                 elif msg["command"] == "get_server_status":
-                    print(123123123123123)
-                    ans = {"to_process": "gui", "from_process": "server", "command": "set_server_status", "data": True if self.process is not None else False}
+                    status = self.is_running()
+                    ans = {"to_process": "gui", "from_process": "server", "command": "set_server_status",
+                           "data": status, "request_id": msg.get("request_id")}
                     self.conn.send(ans)
+
             except EOFError as e:
                 print(self.conn, e)
 
+    def is_running(self):
+        if self.process is None:
+            return False
+        result = self.process.poll()
+        if result is None:
+            return True
+        else:
+            self.process = None
+            return False
 
     def start_server(self):
         with open("program_settings.json", "r", encoding="utf-8") as f:
             settings = json.load(f)
         with open("cores.json", "r", encoding="utf-8") as f:
             core_list = json.load(f)
+        if settings["active_core"] == "":
+            msg = {"to_process": "gui", "from_process": "server", "command": "error_out",
+                           "data": 1}
+            self.conn.send(msg)
+            return
         core = Path(core_list[settings["active_core"]]["core_folder"] + "/" + settings["active_core"] + "_" +
                     core_list[settings["active_core"]]["name"])
         full_core_path = core.absolute()
         server_directory = full_core_path.parent
         print(full_core_path)
 
-        # Запускаем процесс с stdin для отправки команд
         self.process = subprocess.Popen(
             ['java', '-Xmx1024M', '-Xms256M', '-jar', str(full_core_path), 'nogui'],
-            stdin=subprocess.PIPE,  # Добавили stdin
+            stdin=subprocess.PIPE,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             text=True,
-            bufsize=1,  # Построчная буферизация
+            bufsize=1,
             cwd=str(server_directory)
         )
 
-        # Запускаем потоки для чтения вывода
         self.stdout_thread = threading.Thread(
             target=self.read_output,
             args=(self.process.stdout, "STDOUT")
@@ -76,7 +91,6 @@ class ServerSystem:
             args=(self.process.stderr, "STDERR")
         )
 
-        # Делаем потоки демонами, чтобы они завершились с основным потоком
         self.stdout_thread.daemon = True
         self.stderr_thread.daemon = True
 
@@ -84,33 +98,52 @@ class ServerSystem:
         self.stderr_thread.start()
 
     def stop_server(self):
-        self.send_commansds("stop")
-        time.sleep(5)
-        if self.process.poll() is None:
-            self.process.stdin.write('stop\n')
-            time.sleep(5)
-        if self.process.poll() is None:
-            self.process.terminate()
-            time.sleep(2)
+        if self.is_running():
+            try:
+                if self.process.stdin and not self.process.stdin.closed:
+                    self.process.stdin.write("stop\n")
+                    self.process.stdin.flush()
+
+                for _ in range(30):
+                    time.sleep(1)
+                    if not self.is_running():
+                        break
+                else:
+                    self.process.terminate()
+                    time.sleep(5)
+
+                    if self.is_running():
+                        self.process.kill()
+
+                self.process.wait(timeout=10)
+            except:
+                pass
+
+            if self.process:
+                if self.process.stdin and not self.process.stdin.closed:
+                    try:
+                        self.process.stdin.close()
+                    except:
+                        pass
+                self.process = None
 
     def read_output(self, stream, name):
-        """Чтение вывода из потока (stdout или stderr)"""
-        for line in iter(stream.readline, ''):
-            print(f"[{name}] {line}", end='')
-        stream.close()
+        try:
+            for line in iter(stream.readline, ''):
+                print(f"[{name}] {line}", end='')
+        finally:
+            stream.close()
 
-
-    def send_commansds(self, command):
-        """Отправка команд в процесс"""
-
-        if self.process.poll() is None:
+    def send_commands(self, command):
+        print(command)
+        if self.is_running():
+            if not command.endswith('\n'):
+                command += '\n'
             self.process.stdin.write(command)
             self.process.stdin.flush()
         else:
-            print("\nОстановка сервера...")
-            self.process.stdin.write('stop\n')
-            self.process.stdin.flush()
-            self.process.wait()
+            self.process = None
+
 
 def run(conn):
     server = ServerSystem(conn)
