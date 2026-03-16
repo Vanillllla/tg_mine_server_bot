d80d835cba7b63169bad0bcfb66c0cd4fb6a06ee
Vanillllla <108992169+Vanillllla@users.noreply.github.com>
2026-03-16 13:35:22 +0300

    0.2.18

--- Tests/test_5.py
+++
@@ -1,6 +0,0 @@
-def func(required_arg, *args, **kwargs):
-    print(f"Обязательный: {required_arg}")
-    print(f"Остальные позиционные: {args}")
-    print(f"Именованные: {kwargs}")
-
-func("обязательный", 2, 3, tttttt="value")
--- Tests/test_8.py
+++
@@ -1,6 +0,0 @@
-l = {"1": {"name": "1n"},
-     "2": {"name": "2n"},
-     "3": {"name": "3n"},
-     "4": {"name": "4n"}}
-for i in l:
-    print(i, l[i]["name"])
